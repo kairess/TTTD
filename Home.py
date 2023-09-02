9dcf3689b32b35b8a37d51b90a124c4072fe5808
Brad <kairess87@gmail.com>
2023-09-02 20:15:18 +0700

    prompot

--- Home.py
+++ Home.py
@@ -3,6 +3,10 @@ import streamlit.components.v1 as components
 from streamlit_pills import pills
 from streamlit_extras.badges import badge
 import openai
+import pandas as pd
+import random
+import plotly.express as px
+from prompts import *
 
 st.set_page_config(page_title="Ticket to the Dream - AI B&B", layout="wide", page_icon="⭐️")
 
@@ -21,8 +25,8 @@ def mermaid(code):
         </pre>
 
         <script type="module">
-            import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-            mermaid.initialize({{ startOnLoad: true, theme: 'dark' }});
+            import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
+            mermaid.initialize({{ startOnLoad: true, theme: "dark" }});
         </script>
         """
     , height=350)
@@ -32,66 +36,74 @@ config = load_config()
 
 openai.api_key = config["openai_api_key"]
 
+##### Sidebar #####
+with st.sidebar:
+    st.header("Ticket to the Dream", divider="rainbow")
+
+    with st.form("parameters-form"):
+        st.subheader("사용자 입력")
+        job = st.text_input("꿈의 직업")
+        name = st.text_input("이름")
+        country = st.selectbox("국가", ["한국", "미국", "독일", "베트남"])
+        age = st.slider("나이", 10, 30, 15, 1)
+        school = st.selectbox("학교", ["초등학교", "중학교", "대학교"])
+        mbti = st.selectbox("MBTI", ["ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP", "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ"])
+        submit = st.form_submit_button("알아보기")
+
+##### Main #####
+answer1 = st.empty()
+
 gpt_prompt = [{
     "role": "system",
-    "content": "You are a chatbot that suggests career path of user input. The user is a student, so you need to respond kindly and easily."
+    "content": system_prompt
 }]
 
-with st.form("prompt-form"):
-    st.info("프롬프트 맨 앞에 / 를 붙여 ChatGPT에게 미래를 상상하게 할 수 있습니다!", icon="🤖")
-    prompt = st.text_area("Prompt", placeholder="당신의 꿈을 입력해주세요!", key="input")
-    submit_prompt = st.form_submit_button("알아보기")
-
-prompt_helper = st.empty()
-
-if submit_prompt and prompt.strip():
-    prompt = prompt.strip()
-
+if submit and name and job:
     new_gpt_prompt = gpt_prompt.copy()
-    new_gpt_prompt.append({
-        "role": "user",
-        "content": prompt
-    })
+    new_user_prompts = user_prompts.copy()
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=new_gpt_prompt,
-        stream=True)
-    
-    print(response)
+    ### Answer 1 ###
+    with st.spinner("꿈으로 향하는 티켓을 발행 중이에요..."):
+        new_gpt_prompt.append({
+            "role": "user",
+            "content": new_user_prompts[0] % (name, country, age, job, school, mbti)
+        })
 
-    collected_messages = []
-    for chunk in response:
-        chunk_message = chunk['choices'][0]['delta']
+        gpt_response1 = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=new_gpt_prompt,
+            stream=True)
 
-        if "content" in chunk_message:
-            collected_messages.append(chunk_message["content"])
-            gpt_response = ''.join(collected_messages)
-            prompt_helper.markdown(f"🤖 {gpt_response}")
+        collected_messages = []
+        for chunk in gpt_response1:
+            chunk_message = chunk["choices"][0]["delta"]
+            if "content" in chunk_message:
+                collected_messages.append(chunk_message["content"])
+                gpt_response1 = "".join(collected_messages)
+                answer1.markdown(f"🤖 {gpt_response1}")
 
-mermaid("""
-timeline
-  title 의사가 되기까지의 스텝
-  section 중학교
-    1. 중학교 졸업 : 중학교 과정을 이수하고 졸업
-  section 고등학교
-    2. 고등학교 입학 : 고등학교에 입학하고 의학과 관련된 과목에 집중
-    3. 대학 준비 : 대학 입시를 위한 준비와 공부
-    4. 고등학교 졸업 : 고등학교 과정을 이수하고 졸업
-  section 대학교
-    5. 의과대학 입학 : 의과대학에 입학
-    6. 기초 의학 공부 : 의학의 기초 지식 습득
-    7. 임상 실습 : 병원에서의 임상 실습 참여
-    8. 의과대학 졸업 : 의과대학 과정을 이수하고 졸업
-  section 전문의
-    9. 전문의 시험 준비 : 전문의 시험을 위한 공부
-    10. 전문의 자격 취득 : 전문의 시험에 합격하고 자격증 취득
-""")
+        new_gpt_prompt.append({
+            "role": "assistant",
+            "content": gpt_response1,
+        })
 
+    ### Answer 2 ###
+    with st.spinner("꿈으로 향하는 로드맵을 그리는 중이에요..."):
+        new_gpt_prompt.append({
+            "role": "user",
+            "content": new_user_prompts[1] % (name, country, age, job, school, mbti),
+        })
 
-import pandas as pd
-import random
-import plotly.express as px
+        gpt_response2 = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=new_gpt_prompt,
+            stream=False)
+
+        gpt_response2 = gpt_response2["choices"][0]["message"]["content"]
+        print(gpt_response2)
+
+        mermaid(gpt_response2)
+
 
 df = pd.DataFrame(dict(
 r=[random.randint(0,2),
@@ -99,7 +111,7 @@ r=[random.randint(0,2),
     random.randint(0,4),
     random.randint(0,5),
     random.randint(0,6)],
-theta=['processing cost','mechanical properties','chemical stability',
-        'thermal stability', 'device integration']))
-fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+theta=["processing cost","mechanical properties","chemical stability",
+        "thermal stability", "device integration"]))
+fig = px.line_polar(df, r="r", theta="theta", line_close=True)
 st.write(fig)
---
+++ prompts.py
@@ -0,0 +1,46 @@
+system_prompt = """너는 사용자가 원하는 직업을 얻을 수 있게 컨설팅을 해주는 AI이다. 너는 "Ticket To The Dream"이라는 서비스의 AI로서, 사용자가 꿈에 그리는 직업을 얻을 수 있도록 도와주는 AI이다. 
+너는 사용자에게 다양한 방법으로 상세한 로드맵을 제공해야 한다. 사용자의 나이, 학력, MBTI, 국가 등을 고려하여 되고 싶은 직업에 도달하기 위한 단계별 목표와 해야 할 일을 제시하라."""
+
+user_prompts = [
+    """사용자가 입력한 정보를 바탕으로 꿈을 이루기 위한 조언과 컨설팅을 해주자. 현재 어떤 노력을 하면 몇년 후에 어떻게 발전할 수 있는지, 친근하고 따뜻하고 전문적인 말투로 작성해줘.
+
+사용자 정보
+
+- 이름 : %s
+- 국가 : %s
+- 나이 : %s
+- 되고 싶은 직업 : %s
+- 현재 직업 or 학업 : %s
+- MBTI : %s""",
+
+"""결과물은 mermaid 차트의 timeline 형식에 맞춰야 하며, 제목은 이 직업으로 가는 로드맵을 직업에 대한 이미지, 혹은 희망적인 간략한 메시지로 제목을 설정한다.
+각 섹션은 큰 단계를, 마일스톤은 그 섹션을 완성하기 위한 작은 단계를 나타내야 한다. 
+각 섹션에는 최대 2개의 subject milestone을 설정할 수 있으며, subejct milestone은 큰 목표, Goal을 나타내며, 대표적인 단어나 문장으로 표현되어야 한다. 각각의 subject milestone은 시간의 흐름에 맞게 생성해야하며 동일한 텍스트가 들어가서는 안된다. 
+세부 주제(subject)에는 그 마일스톤을 달성하기 위해 사용자가 노력해야 하는 부분들에 대해 구체적인 정보를 포함하며, 소요시간을 넣어주자. subject는 최대 다섯개 까지 추가 될 수 있다. 각 문장에는 :, 를 포함하면 문법적으로 오류가 생기므로 : 를 포함하면 안된다. 대신 - 를 사용한다.
+사용자가 원하는 최종 목표를 달성한 후에도 커리어를 개발 시킬 수 있는 방향을 한단계 추가해 사용자가 꿈에 그리는 직업을 달성한 후에 어떻게 커리어를 바른 방향으로 발전 시킬지 판단할 수 있게 돕는다.
+결과물의 언어는 사용자의 국가의 언어에 따라 작성한다.
+
+출력 형식(mermaid char에 바로 입력 될 수 있게 timeline 내용만 출력) : 
+timeline
+title {title}
+section {section}
+{subjects milestone or goal}
+: {subject} - {content or description, required qualifications, time needed, etc.}
+: {subject} - {content or description, required qualifications, time needed, etc.}
+...
+section {section}
+{subjects milestone or goal}
+: {subject} - {content or description, required qualifications, time needed, etc.}
+: {subject} - {content or description, required qualifications, time needed, etc.}
+...
+
+
+나의 상황과 내가 되고 싶은 직업은 다음과 같다. 내가 이 직업을 가질 수 있는 방법을 제공해줘. 출력은 국가에 맞는 언어로 해줘.
+
+- 이름 : %s
+- 국가 : %s
+- 나이 : %s
+- 되고 싶은 직업 : %s
+- 직업 or 학업 : %s
+- MBTI : %s"""
+]
